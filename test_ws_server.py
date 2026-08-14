50a4786e76e28647a5bf1e2ade758e983b3525d8
ruyunluo <ruyunluo@igs.com.tw>
2026-08-14 14:13:35 +0800

    use PORT env variable

--- test_ws_server.py
+++ test_ws_server.py
@@ -1,8 +1,10 @@
 # test_ws_server.py(放在專案根目錄,純測試用)
+import os
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 from app import application
 
-server = pywsgi.WSGIServer(("0.0.0.0", 8000), application, handler_class=WebSocketHandler)
+port = int(os.environ.get("PORT", 8000))
+server = pywsgi.WSGIServer(("0.0.0.0", port), application, handler_class=WebSocketHandler)
 print("8000port")
 server.serve_forever()
