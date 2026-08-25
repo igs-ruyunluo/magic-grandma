a0c8834938927a0312c1752d94f7afca9e9e09e8
ruyunluo <ruyunluo@igs.com.tw>
2026-08-25 13:53:50 +0800

    fixed:連線問題

--- db/connection.py
+++ db/connection.py
@@ -4,6 +4,8 @@ from pymongo import MongoClient
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+print(f"[DEBUG] MONGO_URI = {MONGO_URI[:40]}")
+
 client = MongoClient(MONGO_URI)
 
 try:
