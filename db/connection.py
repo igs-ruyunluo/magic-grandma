a770173590dede992040f38c7b799c4b0c337140
ruyunluo <ruyunluo@igs.com.tw>
2026-08-25 13:40:48 +0800

    feat:mongo url

--- db/connection.py
+++ db/connection.py
@@ -1,8 +1,16 @@
+import os
 import redis
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-redis_client = redis.Redis(host = 'localhost', port = 6379, db = 0, protocol=2)
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
+REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+client = MongoClient(MONGO_URI)
+
+try:
+    redis_client = redis.from_url(REDIS_URL, protocol=2)
+    redis_client.ping()
+except:
+    redis_client = None  # Redis 不可用時設為 None
 db = client["game_db"]
 
 db.players.create_index("username", unique=True)
